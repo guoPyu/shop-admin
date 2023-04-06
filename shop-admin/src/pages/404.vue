fe2b557d4aaa4a8911224f8ac5276df234c82f4b
guoPyu <2397348157@qq.com>
2023-04-06 18:52:15 +0800

    feat(新功能):使用vite创建项目并进行配置

---
+++ shop-admin/src/pages/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <el-col :sm="12" :lg="6">
+      <el-result
+        icon="warning"
+        title="404页面"
+        sub-title="您查找的页面不存在"
+      >
+        <template #extra>
+          <el-button type="primary">返回上一页</el-button>
+        </template>
+      </el-result>
+    </el-col>
+  </div>
+</template>
